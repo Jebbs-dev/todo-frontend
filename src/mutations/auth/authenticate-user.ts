3426fdb3fdffbb1e125601fa8545d86d0c1af22f
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-25 20:40:03 +0100

    feat: add mutation to authenticate user

---
+++ src/mutations/auth/authenticate-user.ts
@@ -0,0 +1,28 @@
+import { UserProps } from "@/modules/authentication/components/auth-form";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export const useAuthenticateUser = () => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: async (values: UserProps) => {
+      const response = await axios.post(
+        "http://localhost:8080/api/auth",
+        values,
+        { withCredentials: true }
+      );
+      console.log(response.data);
+      return response.data;
+    },
+    onSettled: (data, error) => {
+      if (error) {
+        console.error("Error authenticating user:", error.message);
+      }
+      if (data) {
+        router.push("/");
+      }
+    },
+  });
+};

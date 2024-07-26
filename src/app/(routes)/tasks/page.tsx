5e930b9823cff256f6781c550aa19fba180c00c9
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-26 22:32:32 +0100

    feat: add tasks page

---
+++ src/app/(routes)/tasks/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Layout from "./layout";
+
+
+
+const Home = () => {
+  
+
+  return (
+    <Layout>
+      <h1 className="text-4xl text-white">Welcome</h1>
+    </Layout>
+  );
+};
+
+export default Home;

b9d49b422abb73a771ebcade6b99114b37353d78
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-26 22:37:04 +0100

    feat: add mutation to add new task

---
+++ src/mutations/task/add-task.ts
@@ -0,0 +1,28 @@
+import { UserProps } from "@/modules/authentication/components/auth-form";
+import { TaskProps } from "@/modules/tasks/[taskId]/components/task-form";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export const useCreateTask = () => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: async (values: TaskProps) => {
+      const response = await axios.post(
+        "http://localhost:8080/api/tasks",
+        values
+      );
+      console.log(response.data);
+      return response.data;
+    },
+    onSettled: (data, error) => {
+      if (error) {
+        console.error("Error creating user:", error.message);
+      }
+      // if (data) {
+      //   router.push("/tasks");
+      // }
+    },
+  });
+};

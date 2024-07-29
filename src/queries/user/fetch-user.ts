2e55a45ae510524f8f14f38ef3f943c46683c621
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-29 23:24:39 +0100

    feat: add query to fetch user

---
+++ src/queries/user/fetch-user.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { User } from "../../../types";
+import { useGetUserAuthStatus } from "../auth/get-user-status";
+
+export const useGetUser = () => {
+
+  const { data: authenticatedUser } = useGetUserAuthStatus();
+
+  return useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const response = await axios.get(
+        `http://localhost:8080/api/users/${authenticatedUser._id}`
+        // { withCredentials: true }
+      );
+      console.log(response.data)
+      return response.data;
+    },
+  });
+};

b6bd288acb9f2f400af0211f0c86329f03992e39
Jebbs-dev <ojebiyifulness@gmail.com>
2024-07-30 23:37:29 +0100

    feat: add colums for data table

---
+++ src/modules/tasks/components/columns.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-actions";
+import { Checkbox } from "@/components/ui/checkbox";
+
+export type TaskColumn = {
+  id: string;
+  title: string;
+  status: "Backlog" | "Todo" | "In Progress" | "Done" | "Cancelled";
+  priority: "Low" | "Medium" | "High";
+};
+
+export const columns: ColumnDef<TaskColumn>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    header: "Task",
+    cell: ({ row }) => {
+      const formattedId = `TASK-${row.index + 1}`;
+
+      return <div>{formattedId}</div>;
+    },
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "priority",
+    header: "Priority",
+  },
+  {
+    accessorKey: "action",
+    header: "",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];

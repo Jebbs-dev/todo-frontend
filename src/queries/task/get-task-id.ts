ca711dd3080728501e1baf33601ef095564c916a
Jebbs-dev <ojebiyifulness@gmail.com>
2024-08-06 16:42:42 +0100

    feat: add query to get task id

---
+++ src/queries/task/get-task-id.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Task } from "../../../types";
+import { useGetTask } from "./get-task";
+
+export const useGetTaskId = (taskId: string) => {
+  return useQuery({
+    queryKey: ["tasks", taskId],
+    queryFn: async (): Promise<Task> => {
+      const response = await axios.get<Task>(
+        `http://localhost:8080/api/tasks/${taskId}`
+      );
+
+      if (!response) {
+        throw new Error("Failed to fetch tasks");
+      }
+
+      return response.data;
+    },
+  });
+};

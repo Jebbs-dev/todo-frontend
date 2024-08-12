f1fa1216a198410595d13bfba562a5189238877c
Jebbs-dev <ojebiyifulness@gmail.com>
2024-08-12 23:36:30 +0100

    feat: add combobox component

---
+++ src/modules/tasks/components/task-status-combobox.tsx
@@ -0,0 +1,88 @@
+
+
+import { FunctionComponent, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Status, statuses } from "./table/data/status";
+import { cn } from "@/lib/utils";
+
+interface StatusPopoverProps {}
+
+const StatusPopover: FunctionComponent<StatusPopoverProps> = () => {
+  const [open, setOpen] = useState(false)
+  const [selectedStatus, setSelectedStatus] = useState<Status | null>(
+    null
+  )
+  return (
+    <div>
+      {/* <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-[150px] justify-start"
+          >
+            {selectedStatus ? (
+              <>
+                <selectedStatus.icon className="mr-2 h-4 w-4 shrink-0" />
+                {selectedStatus.label}
+              </>
+            ) : (
+              <>+ Set status</>
+            )}
+          </Button>
+        </PopoverTrigger> */}
+        <PopoverContent className="p-0" side="right" align="start">
+          <Command>
+            <CommandInput placeholder="Change status..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup>
+                {statuses.map((status) => (
+                  <CommandItem
+                    key={status.value}
+                    value={status.value}
+                    onSelect={(value) => {
+                      setSelectedStatus(
+                        statuses.find((priority) => priority.value === value) ||
+                          null
+                      );
+                      setOpen(false);
+                    }}
+                  >
+                    <status.icon
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        status.value === selectedStatus?.value
+                          ? "opacity-100"
+                          : "opacity-40"
+                      )}
+                    />
+                    <span>{status.label}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      {/* </Popover> */}
+    </div>
+  );
+};
+
+export default StatusPopover;

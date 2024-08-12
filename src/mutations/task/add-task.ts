903fdf21e68339b1c3af36c3376d2b3baa5298ae
Jebbs-dev <ojebiyifulness@gmail.com>
2024-08-12 23:35:42 +0100

    feat: add jwt authentication

--- src/mutations/task/add-task.ts
+++ src/mutations/task/add-task.ts
@@ -9,20 +9,28 @@ export const useCreateTask = () => {
 
   return useMutation({
     mutationFn: async (values: TaskProps) => {
+      const storedToken = localStorage.getItem("jwtToken");
+
+      if (!storedToken) {
+        throw new Error("No token found, user is not authenticated");
+      }
+
+      const parsedToken = JSON.parse(storedToken);
+
       const response = await axios.post(
         "http://localhost:8080/api/tasks",
         values,
-        { withCredentials: true }
+        {
+          headers: {
+            Authorization: `Bearer ${parsedToken}`,
+          },
+        }
       );
-      console.log(response.data);
       return response.data;
     },
     onSettled: (data, error) => {
       if (error) {
         console.error("Error creating task:", error.message);
-      }
-      if (data) {
-        router.refresh();
       }
     },
   });

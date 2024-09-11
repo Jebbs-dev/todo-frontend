6c234c4690913a9ea44d96190946453bc206c439
Jebbs-dev <ojebiyifulness@gmail.com>
2024-09-11 19:39:55 +0100

    Update task-data-table.tsx

--- src/modules/tasks/components/table/task-data-table.tsx
+++ src/modules/tasks/components/table/task-data-table.tsx
@@ -252,7 +252,7 @@ export function DataTable<TData, TValue>({
             Next
           </Button>
         </div>
-        <div className="hidden md:flex-1 text-sm text-muted-foreground">
+        <div className="hidden md:block flex-1 text-sm text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} of{" "}
           {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>

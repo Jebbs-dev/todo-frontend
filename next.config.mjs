0127fa25c8abb1e3e7e9aabd53a27e5c007ef7d8
Jebbs-dev <ojebiyifulness@gmail.com>
2024-09-12 11:57:21 +0100

    remove route redirect
    remove route redirect to align with project structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/tasks',
-        permanent: false,
-      },
-    ]
-  },
   transpilePackages: ["geist"],
 };
 

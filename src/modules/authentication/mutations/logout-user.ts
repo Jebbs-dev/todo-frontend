399c6180caf5fa24e838d215e806a4c664efc824
Jebbs-dev <ojebiyifulness@gmail.com>
2024-09-16 13:21:20 +0100

    relocate logout-user mutation for better module separation

--- src/modules/authentication/mutations/logout-user.ts
+++ src/modules/authentication/mutations/logout-user.ts
@@ -1,21 +1,34 @@
-import { UserProps } from "@/modules/authentication/components/forms/auth-form";
-import { useGetUser } from "@/queries/user/fetch-user";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
 export const useLogoutUser = () => {
-  const { data: user } = useGetUser();
 
   const router = useRouter();
 
   return useMutation({
     mutationFn: async () => {
+
+      const storedToken = localStorage.getItem("jwtToken");
+
+      if (!storedToken) {
+        throw new Error("No token found, user is not authenticated");
+      }
+
+      const parsedToken = JSON.parse(storedToken);
+
       const response = await axios.post(
         "http://localhost:8080/api/auth/logout",
         {},
-        { withCredentials: true }
+        {
+          headers: {
+            Authorization: `Bearer ${parsedToken}`,
+          },
+        }
       );
+      
+      localStorage.removeItem("jwtToken");
+
       return response.data;
     },
     onSuccess: () => {
